acabdbfb1fb98a34837d3554bef7a8872eaf143b
Fyreire <77066742+FieryMewtwo@users.noreply.github.com>
2022-01-04 16:04:58 -0500

    Update the Sitrus Berry

--- src/data/pokemon/item_effects.h
+++ src/data/pokemon/item_effects.h
@@ -336,7 +336,7 @@ const u8 gItemEffect_LumBerry[6] = {
 
 const u8 gItemEffect_SitrusBerry[7] = {
     [4] = ITEM4_HEAL_HP,
-    [6] = 30, // Amount of HP to recover
+    [6] = ITEN6_HEAL_QUARTER, // Amount of HP to recover. This code has been altered to update the Sitrus Berry to its effect in Generation IV+.
 };
 
 #define EV_BERRY_FRIENDSHIP_CHANGE          \
