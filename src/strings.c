128103297cfd758a68653a927ccf1022e6d7aefd
Fyreire <77066742+FieryMewtwo@users.noreply.github.com>
2022-02-07 00:03:10 -0500

    Update strings.c

--- src/strings.c
+++ src/strings.c
@@ -1821,3 +1821,4 @@ const u8 gText_Answer[] = _("ANSWER");
 const u8 gText_PokeBalls[] = _("POKé BALLS");
 const u8 gText_Berry[] = _("BERRY");
 const u8 gText_Berries[] = _("BERRIES");
+const u8 gText_AshQty[] = _("SOOT COLLECTED:\n{STR_VAR_1}{PAUSE_UNTIL_PRESS}");
