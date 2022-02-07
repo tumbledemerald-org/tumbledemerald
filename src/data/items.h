a36cf9447906fca4609d5a97f5232dd2d5b4cb4f
Fyreire <77066742+FieryMewtwo@users.noreply.github.com>
2022-02-07 00:02:19 -0500

    Update items.h

--- src/data/items.h
+++ src/data/items.h
@@ -3288,7 +3288,7 @@ const struct Item gItems[] =
         .importance = 1,
         .pocket = POCKET_KEY_ITEMS,
         .type = ITEM_USE_BAG_MENU,
-        .fieldUseFunc = ItemUseOutOfBattle_CannotUse,
+        .fieldUseFunc = ItemUseOutOfBattle_SootSack
     },
 
     [ITEM_BASEMENT_KEY] =
